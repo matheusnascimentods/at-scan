735a168112bf0507e9d8502a635e838128a222e6
Matheus Nascimento <matheusnascimentods@outlook.com>
2026-06-22 22:19:31 -0300

    feat(web): add LandingComponent as initial route

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './features/landing/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  }
+];
